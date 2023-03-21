5203381461e4b6d273336f02586826a232d9d71f
teiturhelgi <58830507+teiturhelgi@users.noreply.github.com>
2023-03-21 10:44:05 +0100

    Update app_main2.c

--- app_main2.c
+++ app_main2.c
@@ -1,10 +1,6 @@
-/* MQTT (over TCP) Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+/* 
+   Output control ESP32
+   Code by Inti Holmfred
 */
 
 #include <stdio.h>
